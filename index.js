bac93aac1cac04c8c1344efec7543f85a5a6bae6
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-30 19:31:00 +0200

    finish step8 of part 3.6 phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :body'))
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
 
 let phonebookArr = [
     { 

dd681f87ffc2c76459ebd4c009f8a4b897e7d130
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-13 15:38:43 +0200

    install cors and modify port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :body'))
+app.use(cors())
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
@@ -94,7 +96,7 @@ app.post('/api/persons/', (request, response) => {
   response.json(phonebookArr)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })

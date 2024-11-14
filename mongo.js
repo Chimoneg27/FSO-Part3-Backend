6b395fa10bd23ac2b4ff46e72311cec27c2fa5bc
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-14 15:07:16 +0200

    finish part 3.12

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
+if (process.argv.length < 3) {
+  console.log("give password as argument")
   process.exit(1)
 }
 
@@ -9,26 +9,45 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url =
-  `mongodb+srv://GarvinChimone:${password}@persons.hf8ny.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=Persons`
+const url = `mongodb+srv://GarvinChimone:${password}@persons.hf8ny.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=Persons`
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false)
 
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: String
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model("Person", personSchema)
 
-const person = new Person({
-  name,
-  number,
-})
+if (name && number) {
+  const person = new Person({
+    name,
+    number
+  })
 
-person.save().then(result => {
-  console.log(`added ${result.name} number ${result.number} to phonebook`)
-  mongoose.connection.close()
-})
+  person
+    .save()
+    .then((result) => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+    .catch((error) => {
+      console.error("Error saving the person", error)
+      mongoose.connection.close()
+    })
+} else {
+  Person.find({})
+    .then((result) => {
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+    .catch((error) => {
+      console.error("Error fething the person", error)
+      mongoose.connection.close()
+    })
+}

fa8ad8624924d0088dbef81e38d62ba4c527345a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-23 17:04:29 +0200

    finish 3.15 and 3.16

--- index.js
+++ index.js
@@ -27,17 +27,24 @@ app.get('/info', (request, response) => {
         `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(note) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  phonebookArr = phonebookArr.filter(person => person.id !== id) 
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response) => {
@@ -56,6 +63,24 @@ app.post('/api/persons/', (request, response) => {
     response.json(savedNote)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
